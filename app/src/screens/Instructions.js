5326859b632e2ed9d039364fd2da2c3ddffd930e
EvanOptAxe <evan.oleary@optaxe.com>
2023-03-04 12:18:01 +0000

    framework for finish meal
    install stack navigator

--- app/src/screens/Instructions.js
+++ app/src/screens/Instructions.js
@@ -1,12 +1,40 @@
 import { useEffect, useState } from "react"
 import { getInstructionsCall, getInstructionEquipmentImage } from "../Constants"
+import React from 'react';
+import {Button, View, Text} from 'react-native';
 
-export default function InstructionsPage({ recipeId }) {
+
+const finishMeal = (pantry, recipeDetails) => {
+
+    const removeItemURL = `${Constants.API_BASE_URL}/pantry`
+
+    // filter unused ingredients
+    const usedIngredients = recipeDetails.usedIngredients
+
+    // loop through every item - get quantity -
+    pantry.forEach(pantryItem => {
+
+        const itemQuantity = pantryItem.quantity
+        
+        const ingredientQuantity = usedIngredients.find(ingredient => ingredient.name == item.name).amount
+
+        const remainingItemAmount = ingredientQuantity - itemQuantity
+
+        // fetch change -- removeItemURL (name, amount, remainingItemAmount)
+
+    })
+
+}
+
+
+export default function InstructionsPage({ recipe, pantry}) {
+
+    const recipeID = recipe.id
 
     const [instructions, setInstructions] = useState([])
     const [loading, setLoaing] = useState(true)
 
-    const instructionsCall = getInstructionsCall(recipeId)
+    const instructionsCall = getInstructionsCall(recipeID)
 
     useEffect(() => {
 
@@ -27,22 +55,41 @@ export default function InstructionsPage({ recipeId }) {
     })
 
     return (
-        <ScrollView>
-            <Text>{!loading ?? "Loading..."}</Text>
-            {
-                instructions ? instructions.steps.map((instructionStep, index) =>
-                    <View key={index}>
-                        <Text>
-                            {`${instructionStep.number}.\n${instructionStep.step}`}
-                        </Text>
-                        <Image
-                            source={{
-                                uri: getInstructionEquipmentImage(recipeId, instructionStep.image)
-                            }}
-                        />
-                    </View>
-                ) : []
-            }
-        </ScrollView>
+
+        <View>
+
+            {/* Start Meal */}
+            <View>
+
+            </View>
+
+            {/* Instructions */}
+            <ScrollView>
+                <Text>{!loading ?? "Loading..."}</Text>
+                {
+                    instructions ? instructions.steps.map((instructionStep, index) =>
+                        <View key={index}>
+                            <Text>
+                                {`${instructionStep.number}.\n${instructionStep.step}`}
+                            </Text>
+                            <Image
+                                source={{
+                                    uri: getInstructionEquipmentImage(recipeId, instructionStep.image)
+                                }}
+                            />
+                        </View>
+                    ) : []
+                }
+            </ScrollView>
+
+            {/* End meal */}
+            <View>
+                <Button 
+                    onPress={finishMeal}
+                />
+
+            </View>
+            
+        </View>
     )
 }
