a7d5f386dbaa0027e1416d96ea2daeafe84c3502
EvanOptAxe <evan.oleary@optaxe.com>
2023-03-04 19:40:35 +0000

    add stack navigation to feed so we can jump to instructions

--- app/App.js
+++ app/App.js
@@ -2,6 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Instructions from "./src/screens/Instructions";
 import Feed from "./src/screens/Feed";
 import Pantry from "./src/screens/Pantry";
 import Blacklist from "./src/screens/Blacklist";
@@ -9,6 +11,17 @@ import Impact from "./src/screens/Impact";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import Foundation from "react-native-vector-icons/Foundation";
+
+const FeedStack = createNativeStackNavigator();
+
+function FeedStackScreen() {
+  return (
+    <FeedStack.Navigator>
+      <FeedStack.Screen name="Feed" component={Feed} />
+      <FeedStack.Screen name="Instructions" component={Instructions} />
+    </FeedStack.Navigator>
+  );
+}
 
 const Tab = createBottomTabNavigator();
 
@@ -18,7 +31,7 @@ export default function App() {
       <Tab.Navigator initialRouteName="Feed">
         <Tab.Screen
           name="Feed"
-          component={Feed}
+          component={FeedStackScreen}
           options={{
             tabBarIcon: ({ color, size }) => (
               <Ionicons name="ios-list" color={color} size={size} />
