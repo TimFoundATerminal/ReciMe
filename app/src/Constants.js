771cbf3d643a314b18f5d2223a590b3ff29fc1b7
EvanOptAxe <evan.oleary@optaxe.com>
2023-03-04 11:45:42 +0000

    update constants
    refactor API tests

--- app/src/Constants.js
+++ app/src/Constants.js
@@ -2,15 +2,25 @@
 
 // Node Express backend constants
 
-  // change this (IPV4 address from ipconfig in command line)
-const IP_ADDRESS = 'xxx.xxx.xxx.xxx';
-const LOCAL_HOST_PORT = '3000';
-export const API_BASE_URL = 'http://' + IP_ADDRESS +':' + LOCAL_HOST_PORT + '/api';
+// change this (IPV4 address from ipconfig in command line)
+const IP_ADDRESS = 'xxx.xxx.xxx.xxx'
+const LOCAL_HOST_PORT = '3000'
 
-// Spoonacular API constants
+// Spponacular
+const API_KEY = '7b6470073c6246c1be8039c48fe00dd4'
 
-const API_KEY = '7b6470073c6246c1be8039c48fe00dd4';
+// base URLs
+export const API_BASE_URL = `http://${IP_ADDRESS}:${LOCAL_HOST_PORT}/api/`
+export const SPOONACULAR_BASE_URL = "https://api.spoonacular.com/recipes"
 
-export function getSpoonacularAPICall(ingredients) {
-    return `https://api.spoonacular.com/recipes/findByIngredients?apiKey=`+API_KEY+`&ingredients=`+ingredients+`&number=10&ranking=2`;
+export function getRecipesCall(ingredients, numberRecipes = 10) {
+    return `${SPOONACULAR_BASE_URL}/findByIngredients?apiKey=${API_KEY}&ingredients=${ingredients}&number=${numberRecipes}&ranking=2`;
+}
+
+export function getInstructionsCall(recipeId) {
+  return `${SPOONACULAR_BASE_URL}/${recipeId}/analyzedInstructions?apiKey=${API_KEY}&stepBreakdown=true`
+}
+
+export function getInstructionEquipmentImage (recipeId, imageName) {
+  return `${SPOONACULAR_BASE_URL}/${recipeId}/${imageName}?apiKey=${API_KEY}`
 }
